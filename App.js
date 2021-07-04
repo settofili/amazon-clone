c75aa9d8e5a39aa6e0d890efb0820a17914d11cd
settofili <settworld@gmail.com>
2021-07-04 21:22:29 +0100

    ready to install material UI

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-
+import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="app">
-      <h1> Hello Kruger Khan, Building Amazon Store!! </h1>
+    <div className="app">   
+   <Header />
+  {/* Home */}
+   
     </div>
   );
 }
