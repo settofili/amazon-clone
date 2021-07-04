4980eb8265f41caaa3907feae456dc014b5d76d0
settofili <settworld@gmail.com>
2021-07-04 19:30:44 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Hello Kruger Khan, Building Amazon Store!! </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBt_S9CvUHBfRfo4N_puhIceCpBNNOWTus",
+    authDomain: "challenge-861e2.firebaseapp.com",
+    projectId: "challenge-861e2",
+    storageBucket: "challenge-861e2.appspot.com",
+    messagingSenderId: "624678708396",
+    appId: "1:624678708396:web:55ad029d1b5e4684b0eb09",
+    measurementId: "G-0E97J19V2E"
+  };
