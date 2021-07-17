15907165d3b9e66219a3c47ba0fbc6a183c9caeb
settofili <settworld@gmail.com>
2021-07-17 20:27:51 +0100

    just built the product page

--- Home.js
+++ Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-
+import Product from "./Product";
 
 function Home() {
     return  (
@@ -8,21 +8,45 @@ function Home() {
             <div className="home__container">
             <img 
             className="home__image"
-            src="" alt="" />
+            src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Beauty_v2_en_US_1x._CB429089975_.jpg" alt="" />
             
             <div className="home__row">
-            {/* Product */}
-            {/* Product */}
+            <Product title="The lean startup"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Laptops_379x304_1X_en_US._SY304_CB418608471_.jpg"
+            rating={3}
+            />
+             <Product title="The lean startup"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Dash_Oculus_1x._SY304_CB667158353_.jpg"
+            rating={3}
+            />
             </div>
 
             <div className="home__row">
-            {/* Product */}
-            {/* Product */}
-            {/* Product */}
+            <Product title="The lean startup"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/July/amazonbasics_520x520._SY304_CB442725065_.jpg"
+            rating={3}
+            />
+             <Product title="The lean startup"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_HomeBedding_Single_Cat_1x._SY304_CB418596953_.jpg"
+            rating={3}
+            />
+             <Product title="The lean startup"
+            price={199.99}
+            image="https://m.media-amazon.com/images/I/41CejTO1rjS._AC_SY200_.jpg"
+            rating={3}
+            />
             </div>
 
             <div className="home__row">
-            {/* Product */}
+            <Product title="The lean startup"
+            price={199.99}
+            image="https://m.media-amazon.com/images/I/41MUBAPIW7L._AC_SY200_.jpg"
+            rating={3}
+            />
             </div>
 
             </div>
--- Product.js
+++ Product.js
@@ -3,10 +3,31 @@ import "./Product.css";
 
 
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
     return (
         <div className="product">
-            
+            <div className="product__info">
+                <p> {title}</p>
+                <p className="product__price"> 
+                <small>$</small>
+                <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>*</p> 
+                    ))}
+                   
+
+                </div>
+            </div>
+            <img 
+            className="image__product"
+            src={image}
+            alt=""/>
+            <button> Add to Basket</button>
+
         </div>
     )
 }
