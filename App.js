bdf0ef3a8bd5991da5076c3fa80bae296dc45c67
settofili <settworld@gmail.com>
2021-07-18 22:13:22 +0100

    building the checkout page

--- App.js
+++ App.js
@@ -2,14 +2,28 @@ import React from "react";
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout"
+import { BrowserRouter as Router, Swwitch, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    //BEM
+    <Router>
     <div className="app">   
-   <Header />
-   <Home />
+    <Switch>
+      <Route path="/checkout">
+          <Header />
+          <Checkout />
+          <h1>I AM A CHECKOUT, SMASH THE LIKE BUTTON</h1>
+    </Route>
+    <Route path="/">
+          <Header />
+          <Home /> 
+    </Route>
+    </Switch>
    
     </div>
+    </Router>
   );
 }
 
